59060146abd8cdbb6325ce74de01399704c8f49f
Ayushi Singh <ayu.s28.ayushi@gmail.com>
2026-06-09 17:05:06 +0530

    clean repo: removed built artifacts and IDE files

--- spring_framework_basics2/src/main/java/com/example/spring_framework_basics/businesscalculationservice/DataService.java
+++
@@ -1,6 +0,0 @@
-package com.example.spring_framework_basics.businesscalculationservice;
-
-public interface DataService {
-	int[] retrieveData();
-}
-
--- src/oops_basics/Object_composition/Book/Review.java
+++
@@ -1,5 +0,0 @@
-package oops_basics.Object_composition.Book;
-
-public class Review {
-
-}
